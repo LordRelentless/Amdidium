4ea22331e693490ce81bad05a84b87523925aec7
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:41:15 -0600

    Update TemporalTerrainRasterizer.java

--- src/main/java/me/lordrelentless/amdidium/renderers/TemporalTerrainRasterizer.java
+++ src/main/java/me/lordrelentless/amdidium/renderers/TemporalTerrainRasterizer.java
@@ -1,16 +1,16 @@
-package me.cortex.nvidium.renderers;
+package me.lordrelentless.amdidium.renderers;
 
-import me.cortex.nvidium.gl.shader.Shader;
-import me.cortex.nvidium.sodiumCompat.ShaderLoader;
-import me.cortex.nvidium.mixin.minecraft.LightMapAccessor;
+import me.lordrelentless.amdidium.gl.shader.Shader;
+import me.lordrelentless.amdidium.sodiumCompat.ShaderLoader;
+import me.lordrelentless.amdidium.mixin.minecraft.LightMapAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL12C;
 import org.lwjgl.opengl.GL45;
 import org.lwjgl.opengl.GL45C;
 
-import static me.cortex.nvidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
-import static me.cortex.nvidium.gl.shader.ShaderType.*;
+import static me.lordrelentless.amdidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.*;
 import static org.lwjgl.opengl.GL11C.*;
 import static org.lwjgl.opengl.GL11C.GL_TEXTURE_WRAP_S;
 import static org.lwjgl.opengl.GL33.glGenSamplers;
@@ -21,15 +21,17 @@ public class TemporalTerrainRasterizer extends Phase {
     private final int blockSampler = glGenSamplers();
     private final int lightSampler = glGenSamplers();
     private final Shader shader = Shader.make()
-            .addSource(TASK, ShaderLoader.parse(Identifier.of("nvidium", "terrain/temporal_task.glsl")))
-            .addSource(MESH, ShaderLoader.parse(Identifier.of("nvidium", "terrain/mesh.glsl")))
-            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("nvidium", "terrain/frag.frag"))).compile();
+            .addSource(TASK, ShaderLoader.parse(Identifier.of("amdidium", "terrain/temporal_task.glsl")))
+            .addSource(MESH, ShaderLoader.parse(Identifier.of("amdidium", "terrain/mesh.glsl")))
+            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("amdidium", "terrain/frag.frag")))
+            .compile();
 
     public TemporalTerrainRasterizer() {
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MIN_LOD, 0);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAX_LOD, 4);
+
         GL45C.glSamplerParameteri(lightSampler, GL_TEXTURE_WRAP_T, GL12C.GL_CLAMP_TO_EDGE);
         GL45C.glSamplerParameteri(lightSampler, GL_TEXTURE_WRAP_S, GL12C.GL_CLAMP_TO_EDGE);
         GL45C.glSamplerParameteri(lightSampler, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -39,8 +41,16 @@ public class TemporalTerrainRasterizer extends Phase {
     public void raster(int regionCount, long commandAddr) {
         shader.bind();
 
-        int blockId = MinecraftClient.getInstance().getTextureManager().getTexture(Identifier.of("minecraft", "textures/atlas/blocks.png")).getGlId();
-        int lightId = ((LightMapAccessor)MinecraftClient.getInstance().gameRenderer.getLightmapTextureManager()).getTexture().getGlId();
+        int blockId = MinecraftClient.getInstance()
+                .getTextureManager()
+                .getTexture(Identifier.of("minecraft", "textures/atlas/blocks.png"))
+                .getGlId();
+
+        int lightId = ((LightMapAccessor) MinecraftClient.getInstance()
+                .gameRenderer
+                .getLightmapTextureManager())
+                .getTexture()
+                .getGlId();
 
         GL45C.glBindTextureUnit(0, blockId);
         GL45C.glBindSampler(0, blockSampler);
@@ -48,11 +58,8 @@ public class TemporalTerrainRasterizer extends Phase {
         GL45C.glBindTextureUnit(1, lightId);
         GL45C.glBindSampler(1, lightSampler);
 
-
-
-        glBufferAddressRangeNV(GL_DRAW_INDIRECT_ADDRESS_NV, 0, commandAddr, regionCount*8L);//Bind the command buffer
-        glMultiDrawMeshTasksIndirectNV( 0, regionCount, 0);
-
+        glBufferAddressRangeNV(GL_DRAW_INDIRECT_ADDRESS_NV, 0, commandAddr, regionCount * 8L);
+        glMultiDrawMeshTasksIndirectNV(0, regionCount, 0);
 
         GL45C.glBindSampler(0, 0);
         GL45C.glBindSampler(1, 0);
