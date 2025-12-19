7ee12738cb49b40478a66eca1b1cf9a7f95bc824
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:28:23 -0600

    Refactor package names and shader paths in RegionRasterizer

--- src/main/java/me/lordrelentless/amdidium/renderers/RegionRasterizer.java
+++ src/main/java/me/lordrelentless/amdidium/renderers/RegionRasterizer.java
@@ -1,23 +1,23 @@
-package me.cortex.nvidium.renderers;
+package me.lordrelentless.amdidium.renderers;
 
-import me.cortex.nvidium.gl.shader.Shader;
-import me.cortex.nvidium.sodiumCompat.ShaderLoader;
+import me.lordrelentless.amdidium.gl.shader.Shader;
+import me.lordrelentless.amdidium.sodiumCompat.ShaderLoader;
 import me.jellysquid.mods.sodium.client.gl.shader.ShaderParser;
 import net.minecraft.util.Identifier;
 
-import static me.cortex.nvidium.gl.shader.ShaderType.FRAGMENT;
-import static me.cortex.nvidium.gl.shader.ShaderType.MESH;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.FRAGMENT;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.MESH;
 import static org.lwjgl.opengl.NVMeshShader.glDrawMeshTasksNV;
 
 public class RegionRasterizer extends Phase {
     private final Shader shader = Shader.make()
-                    .addSource(MESH, ShaderLoader.parse(Identifier.of("nvidium", "occlusion/region_raster/mesh.glsl")))
-                    .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("nvidium", "occlusion/region_raster/fragment.frag")))
-                    .compile();
+            .addSource(MESH, ShaderLoader.parse(Identifier.of("amdidium", "occlusion/region_raster/mesh.glsl")))
+            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("amdidium", "occlusion/region_raster/fragment.frag")))
+            .compile();
 
     public void raster(int regionCount) {
         shader.bind();
-        glDrawMeshTasksNV(0,regionCount);
+        glDrawMeshTasksNV(0, regionCount);
     }
 
     public void delete() {
