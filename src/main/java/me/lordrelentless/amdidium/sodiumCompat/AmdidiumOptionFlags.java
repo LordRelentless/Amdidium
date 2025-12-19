e5a0dcf5653444c271b7cbb69a9e801ad8ba7448
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:56:19 -0600

    Update and rename NvidiumOptionFlags.java to AmdidiumOptionFlags.java

--- src/main/java/me/lordrelentless/amdidium/sodiumCompat/AmdidiumOptionFlags.java
+++ src/main/java/me/lordrelentless/amdidium/sodiumCompat/AmdidiumOptionFlags.java
@@ -1,7 +1,7 @@
-package me.cortex.nvidium.sodiumCompat;
+package me.lordrelentless.amdidium.sodiumCompat;
 
 import me.jellysquid.mods.sodium.client.gui.options.OptionFlag;
 
-public class NvidiumOptionFlags {
+public class AmdidiumOptionFlags {
     public static OptionFlag REQUIRES_SHADER_RELOAD;
 }
