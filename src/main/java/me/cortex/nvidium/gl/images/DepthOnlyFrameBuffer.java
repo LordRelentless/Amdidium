bbd8f3a20803abdfc7414edb9acba7fe989ac5f2
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 16:04:09 -0600

    Refactor DepthOnlyFrameBuffer class for clarity

--- src/main/java/me/cortex/nvidium/gl/images/DepthOnlyFrameBuffer.java
+++ src/main/java/me/cortex/nvidium/gl/images/DepthOnlyFrameBuffer.java
@@ -1,4 +1,4 @@
-package me.cortex.nvidium.gl.images;
+package me.cortex.amdidium.gl.images;
 
 import com.mojang.blaze3d.platform.GlConst;
 import com.mojang.blaze3d.platform.GlStateManager;
@@ -8,36 +8,55 @@ import static org.lwjgl.opengl.GL11C.GL_TEXTURE_2D;
 import static org.lwjgl.opengl.GL11C.glDeleteTextures;
 import static org.lwjgl.opengl.GL30C.*;
 
+/**
+ * OpenGL depth-only framebuffer.
+ *
+ * This is the OpenGL backend implementation used by Amdidium.
+ * Vulkan and DirectX backends will provide their own equivalents.
+ */
 public class DepthOnlyFrameBuffer {
+
     public final int width;
     public final int height;
-    private final int fid;
-    private final int did;
+
+    private final int framebufferId;
+    private final int depthTextureId;
+
     public DepthOnlyFrameBuffer(int width, int height) {
         this.width = width;
         this.height = height;
-        fid = glCreateFramebuffers();
-        did = glCreateTextures(GL_TEXTURE_2D);
-        glTextureStorage2D(did, 1, GL_DEPTH_COMPONENT32F, width, height);
-        glNamedFramebufferTexture(fid, GL_DEPTH_ATTACHMENT, did, 0);
-        if (glCheckNamedFramebufferStatus(fid, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-            throw new IllegalStateException("ERROR: " + glCheckFramebufferStatus(GL_FRAMEBUFFER));
+
+        framebufferId = glCreateFramebuffers();
+        depthTextureId = glCreateTextures(GL_TEXTURE_2D);
+
+        // Allocate depth texture
+        glTextureStorage2D(depthTextureId, 1, GL_DEPTH_COMPONENT32F, width, height);
+
+        // Attach depth texture to FBO
+        glNamedFramebufferTexture(framebufferId, GL_DEPTH_ATTACHMENT, depthTextureId, 0);
+
+        // Validate FBO
+        if (glCheckNamedFramebufferStatus(framebufferId, GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+            throw new IllegalStateException(
+                    "Depth framebuffer incomplete: " +
+                    glCheckFramebufferStatus(GL_FRAMEBUFFER)
+            );
         }
     }
 
-    public int getDepthBuffer() {
-        return did;
+    public int getDepthTexture() {
+        return depthTextureId;
     }
 
     public void bind(boolean setViewport) {
-        GlStateManager._glBindFramebuffer(GlConst.GL_FRAMEBUFFER, fid);
+        GlStateManager._glBindFramebuffer(GlConst.GL_FRAMEBUFFER, framebufferId);
         if (setViewport) {
             GlStateManager._viewport(0, 0, width, height);
         }
     }
 
     public void delete() {
-        glDeleteFramebuffers(fid);
-        glDeleteTextures(did);
+        glDeleteFramebuffers(framebufferId);
+        glDeleteTextures(depthTextureId);
     }
 }
