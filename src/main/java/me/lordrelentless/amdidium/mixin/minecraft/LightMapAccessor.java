eacca9716ec6a49ef47aaf31f2dc65336cb3b43f
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 16:47:06 -0600

    Update LightMapAccessor.java

--- src/main/java/me/lordrelentless/amdidium/mixin/minecraft/LightMapAccessor.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/minecraft/LightMapAccessor.java
@@ -1,4 +1,4 @@
-package me.cortex.nvidium.mixin.minecraft;
+package me.lordrelentless.amdidium.mixin.minecraft;
 
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.texture.NativeImageBackedTexture;
@@ -7,6 +7,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(LightmapTextureManager.class)
 public interface LightMapAccessor {
-    @Accessor()
+    @Accessor
     NativeImageBackedTexture getTexture();
 }
