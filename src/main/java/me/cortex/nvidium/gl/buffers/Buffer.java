0d17652e15541de0998b17bff4f8bc849cec65e9
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 15:57:00 -0600

    Rename package from nvidium to amdidium

--- src/main/java/me/cortex/nvidium/gl/buffers/Buffer.java
+++ src/main/java/me/cortex/nvidium/gl/buffers/Buffer.java
@@ -1,6 +1,6 @@
-package me.cortex.nvidium.gl.buffers;
+package me.cortex.amdidium.gl.buffers;
 
-import me.cortex.nvidium.gl.IResource;
+import me.cortex.amdidium.gl.IResource;
 
 public interface Buffer extends IResource {
     int getId();
