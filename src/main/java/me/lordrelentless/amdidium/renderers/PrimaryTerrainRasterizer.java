41aa6c25ec50f641567ac4b247097ae93a83c080
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:05:38 -0600

    Refactor package names and shader paths

--- src/main/java/me/lordrelentless/amdidium/renderers/PrimaryTerrainRasterizer.java
+++ src/main/java/me/lordrelentless/amdidium/renderers/PrimaryTerrainRasterizer.java
@@ -1,17 +1,17 @@
-package me.cortex.nvidium.renderers;
+package me.lordrelentless.amdidium.renderers;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-import me.cortex.nvidium.gl.shader.Shader;
-import me.cortex.nvidium.sodiumCompat.ShaderLoader;
-import me.cortex.nvidium.mixin.minecraft.LightMapAccessor;
+import me.lordrelentless.amdidium.gl.shader.Shader;
+import me.lordrelentless.amdidium.sodiumCompat.ShaderLoader;
+import me.lordrelentless.amdidium.mixin.minecraft.LightMapAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL12C;
 import org.lwjgl.opengl.GL45;
 import org.lwjgl.opengl.GL45C;
 
-import static me.cortex.nvidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
-import static me.cortex.nvidium.gl.shader.ShaderType.*;
+import static me.lordrelentless.amdidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.*;
 import static org.lwjgl.opengl.GL11C.*;
 import static org.lwjgl.opengl.GL33.glGenSamplers;
 import static org.lwjgl.opengl.NVMeshShader.glMultiDrawMeshTasksIndirectNV;
@@ -21,12 +21,13 @@ public class PrimaryTerrainRasterizer extends Phase {
     private final int blockSampler = glGenSamplers();
     private final int lightSampler = glGenSamplers();
     private final Shader shader = Shader.make()
-            .addSource(TASK, ShaderLoader.parse(Identifier.of("nvidium", "terrain/task.glsl")))
-            .addSource(MESH, ShaderLoader.parse(Identifier.of("nvidium", "terrain/mesh.glsl")))
-            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("nvidium", "terrain/frag.frag"))).compile();
+            .addSource(TASK, ShaderLoader.parse(Identifier.of("amdidium", "terrain/task.glsl")))
+            .addSource(MESH, ShaderLoader.parse(Identifier.of("amdidium", "terrain/mesh.glsl")))
+            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("amdidium", "terrain/frag.frag")))
+            .compile();
 
     public PrimaryTerrainRasterizer() {
-        GL45C.glSamplerParameteri(blockSampler,     GL45C.GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+        GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAG_FILTER, GL_NEAREST);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MIN_LOD, 0);
         GL45C.glSamplerParameteri(blockSampler, GL45C.GL_TEXTURE_MAX_LOD, 4);
@@ -44,16 +45,25 @@ public class PrimaryTerrainRasterizer extends Phase {
     public void raster(int regionCount, long commandAddr) {
         shader.bind();
 
-        int blockId = MinecraftClient.getInstance().getTextureManager().getTexture(Identifier.of("minecraft", "textures/atlas/blocks.png")).getGlId();
-        int lightId = ((LightMapAccessor)MinecraftClient.getInstance().gameRenderer.getLightmapTextureManager()).getTexture().getGlId();
+        int blockId = MinecraftClient.getInstance()
+                .getTextureManager()
+                .getTexture(Identifier.of("minecraft", "textures/atlas/blocks.png"))
+                .getGlId();
+
+        int lightId = ((LightMapAccessor) MinecraftClient.getInstance()
+                .gameRenderer
+                .getLightmapTextureManager())
+                .getTexture()
+                .getGlId();
 
         GL45C.glBindSampler(0, blockSampler);
         GL45C.glBindSampler(1, lightSampler);
         setTexture(blockId, 0);
         setTexture(lightId, 1);
 
-        glBufferAddressRangeNV(GL_DRAW_INDIRECT_ADDRESS_NV, 0, commandAddr, regionCount*8L);//Bind the command buffer
-        glMultiDrawMeshTasksIndirectNV( 0, regionCount, 0);
+        glBufferAddressRangeNV(GL_DRAW_INDIRECT_ADDRESS_NV, 0, commandAddr, regionCount * 8L);
+        glMultiDrawMeshTasksIndirectNV(0, regionCount, 0);
+
         GL45C.glBindSampler(0, 0);
         GL45C.glBindSampler(1, 0);
     }
