aeb21d527391bc32f633b44062d72a564ce2e2f7
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:38:02 -0600

    Update SortRegionSectionPhase.java

--- src/main/java/me/lordrelentless/amdidium/renderers/SortRegionSectionPhase.java
+++ src/main/java/me/lordrelentless/amdidium/renderers/SortRegionSectionPhase.java
@@ -1,16 +1,16 @@
-package me.cortex.nvidium.renderers;
+package me.lordrelentless.amdidium.renderers;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-import me.cortex.nvidium.gl.shader.Shader;
-import me.cortex.nvidium.mixin.minecraft.LightMapAccessor;
-import me.cortex.nvidium.sodiumCompat.ShaderLoader;
+import me.lordrelentless.amdidium.gl.shader.Shader;
+import me.lordrelentless.amdidium.mixin.minecraft.LightMapAccessor;
+import me.lordrelentless.amdidium.sodiumCompat.ShaderLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
 import org.lwjgl.opengl.GL45;
 import org.lwjgl.opengl.GL45C;
 
-import static me.cortex.nvidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
-import static me.cortex.nvidium.gl.shader.ShaderType.*;
+import static me.lordrelentless.amdidium.RenderPipeline.GL_DRAW_INDIRECT_ADDRESS_NV;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.*;
 import static org.lwjgl.opengl.GL11C.GL_NEAREST;
 import static org.lwjgl.opengl.GL11C.GL_NEAREST_MIPMAP_LINEAR;
 import static org.lwjgl.opengl.GL33.glGenSamplers;
@@ -20,7 +20,7 @@ import static org.lwjgl.opengl.NVVertexBufferUnifiedMemory.glBufferAddressRangeN
 
 public class SortRegionSectionPhase extends Phase {
     private final Shader shader = Shader.make()
-            .addSource(COMPUTE, ShaderLoader.parse(Identifier.of("nvidium", "sorting/region_section_sorter.comp")))
+            .addSource(COMPUTE, ShaderLoader.parse(Identifier.of("amdidium", "sorting/region_section_sorter.comp")))
             .compile();
 
     public SortRegionSectionPhase() {
