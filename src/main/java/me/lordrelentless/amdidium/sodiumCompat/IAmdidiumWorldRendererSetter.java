b18494946f3a3682bde7d865ad002f8443091dd0
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:51:52 -0600

    Update and rename INvidiumWorldRendererSetter.java to IAmdidiumWorldRendererSetter.java

---
+++ src/main/java/me/lordrelentless/amdidium/sodiumCompat/IAmdidiumWorldRendererSetter.java
@@ -0,0 +1,7 @@
+package me.lordrelentless.amdidium.sodiumCompat;
+
+import me.lordrelentless.amdidium.AmdidiumWorldRenderer;
+
+public interface IAmdidiumWorldRendererSetter {
+    void setWorldRenderer(AmdidiumWorldRenderer renderer);
+}
