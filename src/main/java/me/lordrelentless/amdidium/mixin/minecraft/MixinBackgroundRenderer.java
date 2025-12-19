b98325e441eee5313f793cf1721f9155a33f2c73
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 16:47:47 -0600

    Rename package and update fog condition check

--- src/main/java/me/lordrelentless/amdidium/mixin/minecraft/MixinBackgroundRenderer.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/minecraft/MixinBackgroundRenderer.java
@@ -1,17 +1,17 @@
-package me.cortex.nvidium.mixin.minecraft;
+package me.lordrelentless.amdidium.mixin.minecraft;
 
-import me.cortex.nvidium.Nvidium;
+import me.lordrelentless.amdidium.Amdidium;
 import net.minecraft.client.render.BackgroundRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(BackgroundRenderer.class)
-
 public class MixinBackgroundRenderer {
+
     @ModifyConstant(method = "applyFog", constant = @Constant(floatValue = 192.0F))
     private static float changeFog(float fog) {
-        if (Nvidium.IS_ENABLED) {
+        if (Amdidium.IS_ENABLED) {
             return 9999999f;
         } else {
             return fog;
