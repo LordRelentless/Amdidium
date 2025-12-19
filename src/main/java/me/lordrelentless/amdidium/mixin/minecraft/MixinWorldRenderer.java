b7c4a64fc883265a6cb45786e5a39b000b203e40
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 16:50:30 -0600

    Refactor MixinWorldRenderer to use Amdidium

--- src/main/java/me/lordrelentless/amdidium/mixin/minecraft/MixinWorldRenderer.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/minecraft/MixinWorldRenderer.java
@@ -1,6 +1,6 @@
-package me.cortex.nvidium.mixin.minecraft;
+package me.lordrelentless.amdidium.mixin.minecraft;
 
-import me.cortex.nvidium.Nvidium;
+import me.lordrelentless.amdidium.Amdidium;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.WorldRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,21 +9,33 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldRenderer.class)
 public class MixinWorldRenderer {
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"), require = 0)
+
+    @Redirect(
+        method = "render",
+        at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"),
+        require = 0
+    )
     private float redirectMax(float a, float b) {
-        if (Nvidium.IS_ENABLED) {
+        if (Amdidium.IS_ENABLED) {
             return a;
         }
         return Math.max(a, b);
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;getViewDistance()F"))
+    @Redirect(
+        method = "render",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;getViewDistance()F")
+    )
     private float changeRD(GameRenderer instance) {
         float viewDistance = instance.getViewDistance();
-        if (Nvidium.IS_ENABLED) {
-            var dist = Nvidium.config.region_keep_distance * 16;
-            return dist == 32 * 16 ? viewDistance : (dist == 256 * 16 ? 9999999 : dist);
+
+        if (Amdidium.IS_ENABLED) {
+            var dist = Amdidium.config.region_keep_distance * 16;
+            return dist == 32 * 16
+                    ? viewDistance
+                    : (dist == 256 * 16 ? 9999999 : dist);
         }
+
         return viewDistance;
     }
 }
