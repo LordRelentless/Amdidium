824b8bc17c15bffe573fd4f624c6ad2babbe5fa5
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:55:23 -0600

    Update and rename NvidiumCompactChunkVertex.java to AmdidiumCompactChunkVertex.java

--- src/main/java/me/lordrelentless/amdidium/sodiumCompat/AmdidiumCompactChunkVertex.java
+++ src/main/java/me/lordrelentless/amdidium/sodiumCompat/AmdidiumCompactChunkVertex.java
@@ -1,5 +1,4 @@
-package me.cortex.nvidium.sodiumCompat;
-
+package me.lordrelentless.amdidium.sodiumCompat;
 
 import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexAttributeFormat;
 import me.jellysquid.mods.sodium.client.gl.attribute.GlVertexFormat;
@@ -12,11 +11,12 @@ import net.caffeinemc.mods.sodium.api.util.ColorU8;
 import net.minecraft.util.math.MathHelper;
 import org.lwjgl.system.MemoryUtil;
 
-public class NvidiumCompactChunkVertex implements ChunkVertexType {
-    public static final GlVertexFormat<ChunkMeshAttribute> VERTEX_FORMAT = new GlVertexFormat<>(ChunkMeshAttribute.class, null, 16);
+public class AmdidiumCompactChunkVertex implements ChunkVertexType {
+    public static final GlVertexFormat<ChunkMeshAttribute> VERTEX_FORMAT =
+            new GlVertexFormat<>(ChunkMeshAttribute.class, null, 16);
 
     public static final int STRIDE = 16;
-    public static final NvidiumCompactChunkVertex INSTANCE = new NvidiumCompactChunkVertex();
+    public static final AmdidiumCompactChunkVertex INSTANCE = new AmdidiumCompactChunkVertex();
 
     private static final int POSITION_MAX_VALUE = 65536;
     public static final int TEXTURE_MAX_VALUE = 32768;
@@ -26,7 +26,6 @@ public class NvidiumCompactChunkVertex implements ChunkVertexType {
     private static final float MODEL_SCALE = MODEL_RANGE / POSITION_MAX_VALUE;
     private static final float MODEL_SCALE_INV = POSITION_MAX_VALUE / MODEL_RANGE;
     private static final float TEXTURE_SCALE = (1.0f / TEXTURE_MAX_VALUE);
-
 
     @Override
     public float getTextureScale() {
@@ -53,20 +52,29 @@ public class NvidiumCompactChunkVertex implements ChunkVertexType {
         return (ptr, material, vertex, sectionIndex) -> {
             int light = compactLight(vertex.light);
 
-            MemoryUtil.memPutInt(ptr + 0, (encodePosition(vertex.x) << 0) | (encodePosition(vertex.y) << 16));
-            MemoryUtil.memPutInt(ptr + 4, (encodePosition(vertex.z) << 0) | (encodeDrawParameters(material) << 16) | ((light&0xFF)<<24));
-            MemoryUtil.memPutInt(ptr + 8, (encodeColor(vertex.color) << 0) | (((light>>8)&0xFF) << 24));
-            MemoryUtil.memPutInt(ptr + 12, encodeTexture(vertex.u, vertex.v));
+            MemoryUtil.memPutInt(ptr + 0,
+                    (encodePosition(vertex.x) << 0) |
+                    (encodePosition(vertex.y) << 16));
+
+            MemoryUtil.memPutInt(ptr + 4,
+                    (encodePosition(vertex.z) << 0) |
+                    (encodeDrawParameters(material) << 16) |
+                    ((light & 0xFF) << 24));
+
+            MemoryUtil.memPutInt(ptr + 8,
+                    (encodeColor(vertex.color) << 0) |
+                    (((light >> 8) & 0xFF) << 24));
+
+            MemoryUtil.memPutInt(ptr + 12,
+                    encodeTexture(vertex.u, vertex.v));
 
             return ptr + STRIDE;
         };
     }
 
-
     private static int compactLight(int light) {
         int sky = MathHelper.clamp((light >>> 16) & 0xFF, 8, 248);
-        int block = MathHelper.clamp((light >>>  0) & 0xFF, 8, 248);
-
+        int block = MathHelper.clamp((light >>> 0) & 0xFF, 8, 248);
         return (block << 0) | (sky << 8);
     }
 
@@ -78,20 +86,21 @@ public class NvidiumCompactChunkVertex implements ChunkVertexType {
         return ((material.bits() & 0xFF) << 0);
     }
 
-
     private static int encodeColor(int color) {
         var brightness = ColorU8.byteToNormalizedFloat(ColorABGR.unpackAlpha(color));
 
-        int r = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackRed(color)) * brightness);
-        int g = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackGreen(color)) * brightness);
-        int b = ColorU8.normalizedFloatToByte(ColorU8.byteToNormalizedFloat(ColorABGR.unpackBlue(color)) * brightness);
+        int r = ColorU8.normalizedFloatToByte(
+                ColorU8.byteToNormalizedFloat(ColorABGR.unpackRed(color)) * brightness);
+        int g = ColorU8.normalizedFloatToByte(
+                ColorU8.byteToNormalizedFloat(ColorABGR.unpackGreen(color)) * brightness);
+        int b = ColorU8.normalizedFloatToByte(
+                ColorU8.byteToNormalizedFloat(ColorABGR.unpackBlue(color)) * brightness);
 
         return ColorABGR.pack(r, g, b, 0x00);
     }
 
-
     private static int encodeTexture(float u, float v) {
         return ((Math.round(u * TEXTURE_MAX_VALUE) & 0xFFFF) << 0) |
-                ((Math.round(v * TEXTURE_MAX_VALUE) & 0xFFFF) << 16);
+               ((Math.round(v * TEXTURE_MAX_VALUE) & 0xFFFF) << 16);
     }
 }
