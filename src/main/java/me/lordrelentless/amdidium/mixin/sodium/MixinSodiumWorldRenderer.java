d7bd24d1d5a64dd68bbc1389b8a5ac18d5bb041c
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:02:25 -0600

    Refactor MixinSodiumWorldRenderer for Amdidium

--- src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinSodiumWorldRenderer.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinSodiumWorldRenderer.java
@@ -1,9 +1,9 @@
-package me.cortex.nvidium.mixin.sodium;
+package me.lordrelentless.amdidium.mixin.sodium;
 
 import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
-import me.cortex.nvidium.Nvidium;
-import me.cortex.nvidium.NvidiumWorldRenderer;
-import me.cortex.nvidium.sodiumCompat.INvidiumWorldRendererGetter;
+import me.lordrelentless.amdidium.Amdidium;
+import me.lordrelentless.amdidium.AmdidiumWorldRenderer;
+import me.lordrelentless.amdidium.sodiumCompat.IAmdidiumWorldRendererGetter;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager;
 import me.jellysquid.mods.sodium.client.render.viewport.Viewport;
@@ -23,39 +23,90 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.SortedSet;
 
 @Mixin(value = SodiumWorldRenderer.class, remap = false)
-public abstract class MixinSodiumWorldRenderer implements INvidiumWorldRendererGetter {
+public abstract class MixinSodiumWorldRenderer implements IAmdidiumWorldRendererGetter {
+
     @Shadow private RenderSectionManager renderSectionManager;
 
     @Shadow
-    protected static void renderBlockEntity(MatrixStack matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions, float tickDelta, VertexConsumerProvider.Immediate immediate, double x, double y, double z, BlockEntityRenderDispatcher dispatcher, BlockEntity entity) {
+    protected static void renderBlockEntity(MatrixStack matrices,
+                                            BufferBuilderStorage bufferBuilders,
+                                            Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions,
+                                            float tickDelta,
+                                            VertexConsumerProvider.Immediate immediate,
+                                            double x, double y, double z,
+                                            BlockEntityRenderDispatcher dispatcher,
+                                            BlockEntity entity) {
     }
 
-    @Inject(method = "setupTerrain", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSectionManager;needsUpdate()Z", shift = At.Shift.BEFORE))
-    private void injectTerrainSetup(Camera camera, Viewport viewport, int frame, boolean spectator, boolean updateChunksImmediately, CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
-            ((INvidiumWorldRendererGetter)renderSectionManager).getRenderer().update(camera, viewport, frame, spectator);
+    @Inject(
+        method = "setupTerrain",
+        at = @At(
+            value = "INVOKE",
+            target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSectionManager;needsUpdate()Z",
+            shift = At.Shift.BEFORE
+        )
+    )
+    private void injectTerrainSetup(Camera camera,
+                                    Viewport viewport,
+                                    int frame,
+                                    boolean spectator,
+                                    boolean updateChunksImmediately,
+                                    CallbackInfo ci) {
+
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
+            ((IAmdidiumWorldRendererGetter) renderSectionManager)
+                    .getRenderer()
+                    .update(camera, viewport, frame, spectator);
         }
     }
 
-    @Inject(method = "renderBlockEntities(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/BufferBuilderStorage;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;FLnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDDLnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;)V", at = @At("HEAD"), cancellable = true, remap = true)
-    private void overrideEntityRenderer(MatrixStack matrices, BufferBuilderStorage bufferBuilders, Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions, float tickDelta, VertexConsumerProvider.Immediate immediate, double x, double y, double z, BlockEntityRenderDispatcher blockEntityRenderer, CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
+    @Inject(
+        method = "renderBlockEntities(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/BufferBuilderStorage;Lit/unimi/dsi/fastutil/longs/Long2ObjectMap;FLnet/minecraft/client/render/VertexConsumerProvider$Immediate;DDDLnet/minecraft/client/render/block/entity/BlockEntityRenderDispatcher;)V",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = true
+    )
+    private void overrideEntityRenderer(MatrixStack matrices,
+                                        BufferBuilderStorage bufferBuilders,
+                                        Long2ObjectMap<SortedSet<BlockBreakingInfo>> blockBreakingProgressions,
+                                        float tickDelta,
+                                        VertexConsumerProvider.Immediate immediate,
+                                        double x, double y, double z,
+                                        BlockEntityRenderDispatcher blockEntityRenderer,
+                                        CallbackInfo ci) {
+
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             ci.cancel();
-            var sectionsWithEntities = ((INvidiumWorldRendererGetter)renderSectionManager).getRenderer().getSectionsWithEntities();
+
+            var sectionsWithEntities =
+                    ((IAmdidiumWorldRendererGetter) renderSectionManager)
+                            .getRenderer()
+                            .getSectionsWithEntities();
+
             for (var section : sectionsWithEntities) {
                 if (section.isDisposed() || section.getCulledBlockEntities() == null)
                     continue;
+
                 for (var entity : section.getCulledBlockEntities()) {
-                    renderBlockEntity(matrices, bufferBuilders, blockBreakingProgressions, tickDelta, immediate, x, y, z, blockEntityRenderer, entity);
+                    renderBlockEntity(
+                            matrices,
+                            bufferBuilders,
+                            blockBreakingProgressions,
+                            tickDelta,
+                            immediate,
+                            x, y, z,
+                            blockEntityRenderer,
+                            entity
+                    );
                 }
             }
         }
     }
 
     @Override
-    public NvidiumWorldRenderer getRenderer() {
-        if (Nvidium.IS_ENABLED) {
-            return ((INvidiumWorldRendererGetter)renderSectionManager).getRenderer();
+    public AmdidiumWorldRenderer getRenderer() {
+        if (Amdidium.IS_ENABLED) {
+            return ((IAmdidiumWorldRendererGetter) renderSectionManager).getRenderer();
         } else {
             return null;
         }
