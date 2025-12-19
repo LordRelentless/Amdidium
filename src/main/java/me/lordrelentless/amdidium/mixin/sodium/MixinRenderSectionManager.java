50be7239b5a3c504376b0abe94baa9f7412cf806
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 17:00:43 -0600

    Refactor MixinRenderSectionManager for Amdidium

--- src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinRenderSectionManager.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinRenderSectionManager.java
@@ -1,10 +1,10 @@
-package me.cortex.nvidium.mixin.sodium;
+package me.lordrelentless.amdidium.mixin.sodium;
 
 import it.unimi.dsi.fastutil.longs.Long2ReferenceMap;
-import me.cortex.nvidium.Nvidium;
-import me.cortex.nvidium.NvidiumWorldRenderer;
-import me.cortex.nvidium.managers.AsyncOcclusionTracker;
-import me.cortex.nvidium.sodiumCompat.*;
+import me.lordrelentless.amdidium.Amdidium;
+import me.lordrelentless.amdidium.AmdidiumWorldRenderer;
+import me.lordrelentless.amdidium.managers.AsyncOcclusionTracker;
+import me.lordrelentless.amdidium.sodiumCompat.*;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gl.device.CommandList;
 import me.jellysquid.mods.sodium.client.render.chunk.ChunkRenderMatrices;
@@ -35,55 +35,80 @@ import java.util.Collection;
 import java.util.Map;
 
 @Mixin(value = RenderSectionManager.class, remap = false)
-public class MixinRenderSectionManager implements INvidiumWorldRendererGetter {
+public class MixinRenderSectionManager implements IAmdidiumWorldRendererGetter {
+
     @Shadow @Final private RenderRegionManager regions;
     @Shadow @Final private Long2ReferenceMap<RenderSection> sectionByPosition;
     @Shadow private @NotNull Map<ChunkUpdateType, ArrayDeque<RenderSection>> rebuildLists;
     @Shadow @Final private int renderDistance;
-    @Unique private NvidiumWorldRenderer renderer;
+
+    @Unique private AmdidiumWorldRenderer renderer;
     @Unique private Viewport viewport;
 
     @Unique
-    private static void updateNvidiumIsEnabled() {
-        Nvidium.IS_ENABLED = (!Nvidium.FORCE_DISABLE) && Nvidium.IS_COMPATIBLE && IrisCheck.checkIrisShouldDisable();
+    private static void updateAmdidiumIsEnabled() {
+        Amdidium.IS_ENABLED =
+                (!Amdidium.FORCE_DISABLE)
+                && Amdidium.IS_COMPATIBLE
+                && IrisCheck.checkIrisShouldDisable();
     }
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void init(ClientWorld world, int renderDistance, CommandList commandList, CallbackInfo ci) {
-        updateNvidiumIsEnabled();
-        if (Nvidium.IS_ENABLED) {
+        updateAmdidiumIsEnabled();
+        if (Amdidium.IS_ENABLED) {
             if (renderer != null)
                 throw new IllegalStateException("Cannot have multiple world renderers");
-            renderer = new NvidiumWorldRenderer(Nvidium.config.async_bfs?new AsyncOcclusionTracker(renderDistance, sectionByPosition, world, rebuildLists):null);
-            ((INvidiumWorldRendererSetter)regions).setWorldRenderer(renderer);
+
+            renderer = new AmdidiumWorldRenderer(
+                    Amdidium.config.async_bfs
+                            ? new AsyncOcclusionTracker(renderDistance, sectionByPosition, world, rebuildLists)
+                            : null
+            );
+
+            ((IAmdidiumWorldRendererSetter) regions).setWorldRenderer(renderer);
         }
     }
 
-    @ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/compile/executor/ChunkBuilder;<init>(Lnet/minecraft/client/world/ClientWorld;Lme/jellysquid/mods/sodium/client/render/chunk/vertex/format/ChunkVertexType;)V", remap = true), index = 1)
+    @ModifyArg(
+            method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/compile/executor/ChunkBuilder;<init>(Lnet/minecraft/client/world/ClientWorld;Lme/jellysquid/mods/sodium/client/render/chunk/vertex/format/ChunkVertexType;)V",
+                    remap = true
+            ),
+            index = 1
+    )
     private ChunkVertexType modifyVertexType(ChunkVertexType vertexType) {
-        updateNvidiumIsEnabled();
-        if (Nvidium.IS_ENABLED) {
-            return NvidiumCompactChunkVertex.INSTANCE;
+        updateAmdidiumIsEnabled();
+        if (Amdidium.IS_ENABLED) {
+            return AmdidiumCompactChunkVertex.INSTANCE;
         }
         return vertexType;
     }
 
-
     @Inject(method = "destroy", at = @At("TAIL"))
     private void destroy(CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED) {
+        if (Amdidium.IS_ENABLED) {
             if (renderer == null)
                 throw new IllegalStateException("Pipeline already destroyed");
-            ((INvidiumWorldRendererSetter)regions).setWorldRenderer(null);
+
+            ((IAmdidiumWorldRendererSetter) regions).setWorldRenderer(null);
             renderer.delete();
             renderer = null;
         }
     }
 
-    @Redirect(method = "onSectionRemoved", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;delete()V"))
+    @Redirect(
+            method = "onSectionRemoved",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;delete()V"
+            )
+    )
     private void deleteSection(RenderSection section) {
-        if (Nvidium.IS_ENABLED) {
-            if (Nvidium.config.region_keep_distance == 32) {
+        if (Amdidium.IS_ENABLED) {
+            if (Amdidium.config.region_keep_distance == 32) {
                 renderer.deleteSection(section);
             }
         }
@@ -97,21 +122,23 @@ public class MixinRenderSectionManager implements INvidiumWorldRendererGetter {
 
     @Inject(method = "renderLayer", at = @At("HEAD"), cancellable = true)
     public void renderLayer(ChunkRenderMatrices matrices, TerrainRenderPass pass, double x, double y, double z, CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED) {
+        if (Amdidium.IS_ENABLED) {
             ci.cancel();
             pass.startDrawing();
+
             if (pass == DefaultTerrainRenderPasses.SOLID) {
                 renderer.renderFrame(viewport, matrices, x, y, z);
             } else if (pass == DefaultTerrainRenderPasses.TRANSLUCENT) {
                 renderer.renderTranslucent();
             }
+
             pass.endDrawing();
         }
     }
 
     @Inject(method = "getDebugStrings", at = @At("HEAD"), cancellable = true)
     private void redirectDebug(CallbackInfoReturnable<Collection<String>> cir) {
-        if (Nvidium.IS_ENABLED) {
+        if (Amdidium.IS_ENABLED) {
             var debugStrings = new ArrayList<String>();
             renderer.addDebugInfo(debugStrings);
             cir.setReturnValue(debugStrings);
@@ -120,61 +147,85 @@ public class MixinRenderSectionManager implements INvidiumWorldRendererGetter {
     }
 
     @Override
-    public NvidiumWorldRenderer getRenderer() {
+    public AmdidiumWorldRenderer getRenderer() {
         return renderer;
     }
 
     @Inject(method = "createTerrainRenderList", at = @At("HEAD"), cancellable = true)
     private void redirectTerrainRenderList(Camera camera, Viewport viewport, int frame, boolean spectator, CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             ci.cancel();
         }
     }
 
-    @Redirect(method = "submitRebuildTasks", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;setPendingUpdate(Lme/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType;)V"))
+    @Redirect(
+            method = "submitRebuildTasks",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;setPendingUpdate(Lme/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType;)V"
+            )
+    )
     private void injectEnqueueFalse(RenderSection instance, ChunkUpdateType type) {
         instance.setPendingUpdate(type);
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
-            //We need to reset the fact that its been submitted to the rebuild queue from the build queue
+
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             ((IRenderSectionExtension) instance).isSubmittedRebuild(false);
         }
     }
 
     @Unique
     private boolean isSectionVisibleBfs(RenderSection section) {
-        //The reason why this is done is that since the bfs search is async it could be updating the frame counter with the next frame
-        // while some sections that arnt updated/ticked yet still have the old frame id
         int delta = Math.abs(section.getLastVisibleFrame() - renderer.getAsyncFrameId());
         return delta <= 1;
     }
 
-    @Inject(method = "isSectionVisible", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;getLastVisibleFrame()I", shift = At.Shift.BEFORE), cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(
+            method = "isSectionVisible",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;getLastVisibleFrame()I",
+                    shift = At.Shift.BEFORE
+            ),
+            cancellable = true,
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
     private void redirectIsSectionVisible(int x, int y, int z, CallbackInfoReturnable<Boolean> cir, RenderSection render) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             cir.setReturnValue(isSectionVisibleBfs(render));
         }
     }
 
     @Inject(method = "tickVisibleRenders", at = @At("HEAD"), cancellable = true)
     private void redirectAnimatedSpriteUpdates(CallbackInfo ci) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs && SodiumClientMod.options().performance.animateOnlyVisibleTextures) {
+        if (Amdidium.IS_ENABLED
+                && Amdidium.config.async_bfs
+                && SodiumClientMod.options().performance.animateOnlyVisibleTextures) {
+
             ci.cancel();
             var sprites = renderer.getAnimatedSpriteSet();
-            if (sprites == null) {
-                return;
-            }
+            if (sprites == null) return;
+
             for (var sprite : sprites) {
                 SpriteUtil.markSpriteActive(sprite);
             }
         }
     }
 
-    @Inject(method = "scheduleRebuild", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;setPendingUpdate(Lme/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(
+            method = "scheduleRebuild",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSection;setPendingUpdate(Lme/jellysquid/mods/sodium/client/render/chunk/ChunkUpdateType;)V",
+                    shift = At.Shift.AFTER
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
     private void instantReschedule(int x, int y, int z, boolean important, CallbackInfo ci, RenderSection section, ChunkUpdateType pendingUpdate) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             var queue = rebuildLists.get(pendingUpdate);
+
             if (isSectionVisibleBfs(section) && queue.size() < pendingUpdate.getMaximumQueueSize()) {
-                ((IRenderSectionExtension)section).isSubmittedRebuild(true);
+                ((IRenderSectionExtension) section).isSubmittedRebuild(true);
                 rebuildLists.get(pendingUpdate).add(section);
             }
         }
@@ -182,7 +233,7 @@ public class MixinRenderSectionManager implements INvidiumWorldRendererGetter {
 
     @Inject(method = "getVisibleChunkCount", at = @At("HEAD"), cancellable = true)
     private void injectVisibilityCount(CallbackInfoReturnable<Integer> cir) {
-        if (Nvidium.IS_ENABLED && Nvidium.config.async_bfs) {
+        if (Amdidium.IS_ENABLED && Amdidium.config.async_bfs) {
             cir.setReturnValue(this.renderer.getAsyncBfsVisibilityCount());
         }
     }
