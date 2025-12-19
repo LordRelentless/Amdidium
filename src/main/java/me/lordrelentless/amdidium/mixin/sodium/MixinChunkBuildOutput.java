a74e2882bcd41d5d2903a9395626106383ea605f
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-19 16:51:40 -0600

    Rename package and update imports in MixinChunkBuildOutput

--- src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinChunkBuildOutput.java
+++ src/main/java/me/lordrelentless/amdidium/mixin/sodium/MixinChunkBuildOutput.java
@@ -1,7 +1,7 @@
-package me.cortex.nvidium.mixin.sodium;
+package me.lordrelentless.amdidium.mixin.sodium;
 
-import me.cortex.nvidium.sodiumCompat.IRepackagedResult;
-import me.cortex.nvidium.sodiumCompat.RepackagedSectionOutput;
+import me.lordrelentless.amdidium.sodiumCompat.IRepackagedResult;
+import me.lordrelentless.amdidium.sodiumCompat.RepackagedSectionOutput;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildOutput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -11,12 +11,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ChunkBuildOutput.class, remap = false)
 public class MixinChunkBuildOutput implements IRepackagedResult {
-    @Unique private RepackagedSectionOutput repackagedSectionOutput;
 
+    @Unique
+    private RepackagedSectionOutput repackagedSectionOutput;
+
+    @Override
     public RepackagedSectionOutput getOutput() {
         return repackagedSectionOutput;
     }
 
+    @Override
     public void set(RepackagedSectionOutput output) {
         repackagedSectionOutput = output;
     }
