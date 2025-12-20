af350541aea9799b011e51989114540de85887fe
LordRelentless <94130636+LordRelentless@users.noreply.github.com>
2025-12-20 14:53:53 -0600

    Update RegionRasterizer.java

--- src/main/java/me/lordrelentless/amdidium/renderers/RegionRasterizer.java
+++ src/main/java/me/lordrelentless/amdidium/renderers/RegionRasterizer.java
@@ -2,25 +2,37 @@ package me.lordrelentless.amdidium.renderers;
 
 import me.lordrelentless.amdidium.gl.shader.Shader;
 import me.lordrelentless.amdidium.sodiumCompat.ShaderLoader;
-import me.jellysquid.mods.sodium.client.gl.shader.ShaderParser;
 import net.minecraft.util.Identifier;
 
-import static me.lordrelentless.amdidium.gl.shader.ShaderType.FRAGMENT;
-import static me.lordrelentless.amdidium.gl.shader.ShaderType.MESH;
+import static me.lordrelentless.amdidium.gl.shader.ShaderType.*;
+import static org.lwjgl.opengl.GL11C.*;
 import static org.lwjgl.opengl.NVMeshShader.glDrawMeshTasksNV;
 
 public class RegionRasterizer extends Phase {
-    private final Shader shader = Shader.make()
+    private final Shader shaderNV = Shader.make()
             .addSource(MESH, ShaderLoader.parse(Identifier.of("amdidium", "occlusion/region_raster/mesh.glsl")))
             .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("amdidium", "occlusion/region_raster/fragment.frag")))
             .compile();
 
-    public void raster(int regionCount) {
+    private final Shader shaderGL = Shader.make()
+            .addSource(VERTEX, ShaderLoader.parse(Identifier.of("amdidium", "terrain/translucent/vertex.glsl")))
+            .addSource(FRAGMENT, ShaderLoader.parse(Identifier.of("amdidium", "occlusion/region_raster/fragment.frag")))
+            .compile();
+
+    public void raster(int regionCount, int commandBufferId, boolean isNvidia) {
+        Shader shader = isNvidia ? shaderNV : shaderGL;
         shader.bind();
-        glDrawMeshTasksNV(0, regionCount);
+
+        if (isNvidia) {
+            glDrawMeshTasksNV(0, regionCount);
+        } else {
+            org.lwjgl.opengl.GL45C.glBindBuffer(org.lwjgl.opengl.GL45C.GL_DRAW_INDIRECT_BUFFER, commandBufferId);
+            org.lwjgl.opengl.GL43C.glMultiDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_INT, 0, regionCount, 20);
+        }
     }
 
     public void delete() {
-        shader.delete();
+        shaderNV.delete();
+        shaderGL.delete();
     }
 }
